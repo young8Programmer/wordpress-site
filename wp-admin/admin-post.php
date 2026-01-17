ba646ff1aeaf66a8af7735fc9f9d476259e8e0fd
Professional Commits <commits@professional.dev>
2026-01-18 01:01:02 +0500

    style: kod formatlash va indentatsiya

--- wp-admin/admin-post.php
+++ wp-admin/admin-post.php
@@ -7,35 +7,26 @@
  * @package WordPress
  * @subpackage Administration
  */
-
 /** We are located in WordPress Administration Screens */
 if ( ! defined( 'WP_ADMIN' ) ) {
 	define( 'WP_ADMIN', true );
 }
-
 if ( defined( 'ABSPATH' ) ) {
 	require_once ABSPATH . 'wp-load.php';
 } else {
 	require_once dirname( __DIR__ ) . '/wp-load.php';
 }
-
 /** Allow for cross-domain requests (from the front end). */
 send_origin_headers();
-
 require_once ABSPATH . 'wp-admin/includes/admin.php';
-
 nocache_headers();
-
 /** This action is documented in wp-admin/admin.php */
 do_action( 'admin_init' );
-
 $action = ! empty( $_REQUEST['action'] ) ? $_REQUEST['action'] : '';
-
 // Reject invalid parameters.
 if ( ! is_scalar( $action ) ) {
 	wp_die( '', 400 );
 }
-
 if ( ! is_user_logged_in() ) {
 	if ( empty( $action ) ) {
 		/**
@@ -49,7 +40,6 @@ if ( ! is_user_logged_in() ) {
 		if ( ! has_action( "admin_post_nopriv_{$action}" ) ) {
 			wp_die( '', 400 );
 		}
-
 		/**
 		 * Fires on a non-authenticated admin post request for the given action.
 		 *
@@ -73,7 +63,6 @@ if ( ! is_user_logged_in() ) {
 		if ( ! has_action( "admin_post_{$action}" ) ) {
 			wp_die( '', 400 );
 		}
-
 		/**
 		 * Fires on an authenticated admin post request for the given action.
 		 *
